fe751bc100841c62b66305dee46b1041f9cd16b5
Lucas <aranchipe98@gmail.com>
2022-08-26 11:35:39 -0300

    variaveis de ambiente

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,13 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'aranchipe1998',
-        database: 'apiuniversidades',
-        /* ssl: {
-            rejectUnauthorized: false
-        } */
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        /*  ssl: {
+             rejectUnauthorized: false
+         } */
     }
 });
 
